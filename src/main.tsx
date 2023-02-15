72a6deecb2fd49ed5654f57b9b5266d94f9cb19d
Bernardo Borges <bernardobbm334@gmail.com>
2023-02-15 18:59:32 -0300

    adding page routing with react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './global.css';
-import { Tweet } from './components/Tweet/Tweet';
 import { Sidebar } from './components/Sidebar/Sidebar';
-import { Header } from './components/Header/Header';
-import { Separator } from './components/Separator/Separator';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
@@ -13,32 +12,7 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 			<Sidebar />
 
 			<div className="content">
-				<main className="timeline">
-					<Header title="Home" />
-
-					<form className="new-tweet-form">
-						<label htmlFor="tweet">
-							<img
-								src="https://github.com/bernardobbm.png"
-								alt="Bernardo Borges"
-							/>
-
-							<textarea
-								id="tweet"
-								placeholder="What's happening?"
-							/>
-						</label>
-
-						<button type="submit">Tweet</button>
-					</form>
-
-					<Separator />
-
-					<Tweet />
-					<Tweet />
-					<Tweet />
-					<Tweet />
-				</main>
+				<RouterProvider router={router} />
 			</div>
 		</div>
 	</React.StrictMode>
